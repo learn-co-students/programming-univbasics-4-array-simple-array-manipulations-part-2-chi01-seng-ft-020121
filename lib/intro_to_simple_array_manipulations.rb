c36c557a4cb360e1908c7485f7404990a6bebece
Guilherme Ehrat Zils <guilhermezils@icloud.com>
2020-12-29 21:13:33 +0000

    Done.

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -3,13 +3,11 @@ require 'pry'
 
 def using_concat (my_favorite_things, more_favs)
     all_my_favs = my_favorite_things.concat(more_favs)
-
 end
 
   
 def using_insert(list_of_programming_languages, another_language)
 new_array = list_of_programming_languages.insert(4,another_language)
-  
 end
   
 
@@ -31,11 +29,8 @@ end
    
    
 def using_delete_at(famous_robots,deleted_robot)
- # binding.pry
   famous_robots = ["Johnny 5", "R2D2", "Robocop"]
     deleted_robot = famous_robots.delete_at(2)
-#binding.pry
-
 end
    
 
